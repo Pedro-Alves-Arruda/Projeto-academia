85d7c07bd4a244e5b161591a30658acdbf1e5f7e
Pedro-Alves-Arruda <pedro.alves.arruda10@gmail.com>
2021-03-22 16:51:54 -0300

    Adicionando o HTML, CSS, JS e mais
    Adicionando o HTML, CSS, JS e mais

---
+++ JAVASCRIPT/mai.js
@@ -0,0 +1,10 @@
+const meuCabecalho = document.querySelector('h1');
+meuCabecalho.textContent = 'Ola Mundo!!!!!';
+
+function compararNumeros(){
+    let num1 = 10;
+    let num2 = '10';
+    return console.log(num1 === num2);
+}
+
+compararNumeros();
